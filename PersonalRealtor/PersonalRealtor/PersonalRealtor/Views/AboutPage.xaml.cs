c31974cffbe35a3691856985a727d36af86bd478
Jayme Rutkoski <jaymerut@gmail.com>
2021-03-21 06:19:49 -0700

    Created Project

---
+++ PersonalRealtor/PersonalRealtor/PersonalRealtor/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PersonalRealtor.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PersonalRealtor/PersonalRealtor/PersonalRealtor/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PersonalRealtor.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PersonalRealtor.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
