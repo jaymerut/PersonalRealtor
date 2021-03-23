70da754cb0d70ea970fb66d6cec4c4f509d8d5a3
Jayme Rutkoski <jaymerut@gmail.com>
2021-03-22 20:26:12 -0700

    Added properties to RealtorListingsRequest model

--- PersonalRealtor/PersonalRealtor/PersonalRealtor/Network/RealtorAPI/Models/RealtorListingsRequest.cs
+++ PersonalRealtor/PersonalRealtor/PersonalRealtor/Network/RealtorAPI/Models/RealtorListingsRequest.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using PersonalRealtor.Models;
 
 namespace PersonalRealtor.Network.RealtorAPI.Models
 {
     public class RealtorListingsRequest
     {
-        // TODO
+        [JsonProperty("fulfillment_ids")]
+        public List<int> FulfillmentIds { get; set; }
+        [JsonProperty("agents")]
+        public List<Agent> Agents { get; set; }
+        [JsonProperty("page")]
+        public int Page { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
     }
 }
