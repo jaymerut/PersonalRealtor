1b704e66f0f576ee5cc97cf6c3058953b0c66920
Jayme Rutkoski <jaymerut@gmail.com>
2021-03-23 20:10:09 -0700

    Added PropertyDetailsResponse

---
+++ PersonalRealtor/PersonalRealtor/PersonalRealtor/Models/PropertyDetailAddress.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalRealtor.Models
+{
+    public class PropertyDetailAddress
+    {
+        [JsonProperty("city")]
+        public string City { get; set; }
+        [JsonProperty("line")]
+        public string Line { get; set; }
+        [JsonProperty("county")]
+        public string County { get; set; }
+        [JsonProperty("county_needed_for_uniq")]
+        public bool CountyNeededForUniq { get; set; }
+        [JsonProperty("state")]
+        public string State { get; set; }
+        [JsonProperty("postal_code")]
+        public string PostalCode { get; set; }
+        [JsonProperty("state_code")]
+        public string StateCode { get; set; }
+        [JsonProperty("address_validation_code")]
+        public string AddressValidationCode { get; set; }
+        [JsonProperty("time_zone")]
+        public string TimeZone { get; set; }
+        [JsonProperty("lat")]
+        public float Latitude { get; set; }
+        [JsonProperty("lon")]
+        public float Longitude { get; set; }
+    }
+}
---
+++ PersonalRealtor/PersonalRealtor/PersonalRealtor/Models/PropertyDetailsProp.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalRealtor.Models
+{
+    public class PropertyDetailsProp
+    {
+        [JsonProperty("property_id")]
+        public string PropertyId { get; set; }
+        [JsonProperty("prop_status")]
+        public string PropStatus { get; set; }
+        [JsonProperty("listing_id")]
+        public string ListingId { get; set; }
+        [JsonProperty("prop_type")]
+        public string PropType { get; set; }
+        [JsonProperty("list_date")]
+        public DateTime ListDate { get; set; }
+        [JsonProperty("hoa_fee")]
+        public int HOAFee { get; set; }
+        [JsonProperty("list_update")]
+        public DateTime ListUpdate { get; set; }
+        [JsonProperty("year_built")]
+        public int YearBuilt { get; set; }
+        [JsonProperty("listing_status")]
+        public string ListingStatus { get; set; }
+        [JsonProperty("beds")]
+        public int Beds { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("baths_full")]
+        public int BathsFull { get; set; }
+        [JsonProperty("stories")]
+        public int Stories { get; set; }
+        /*
+        [JsonProperty("schools")]
+        public List<School> Schools { get; set; 
+        */
+        [JsonProperty("heating")]
+        public string Heating { get; set; }
+        [JsonProperty("cooling")]
+        public string Cooling { get; set; }
+        [JsonProperty("feature_tags")]
+        public List<string> FeatureTags { get; set; }
+        [JsonProperty("address")]
+        public PropertyDetailAddress Address { get; set; }
+        [JsonProperty("features")]
+        public List<PropertyDetailFeature> Features { get; set; }
+        [JsonProperty("photos")]
+        public List<Photo> Photos { get; set; }
+    }
+}
