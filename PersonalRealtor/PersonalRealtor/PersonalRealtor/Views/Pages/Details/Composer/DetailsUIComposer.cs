27307030ad7e98dd9425c0835e2d6a70ee728e2a
Jayme Rutkoski <jaymerut@gmail.com>
2021-05-02 13:38:19 -0700

    Implement skeletons fo UIComposer and Page

---
+++ PersonalRealtor/PersonalRealtor/PersonalRealtor/Views/Pages/Details/Composer/DetailsUIComposer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using PersonalRealtor.Views.Pages.Details.UI;
+
+namespace PersonalRealtor.Views.Pages.Details.Composer
+{
+    public static class DetailsUIComposer
+    {
+        public static DetailsPage MakeDetailsUI()
+        {
+            return new DetailsPage();
+        }
+    }
+}
