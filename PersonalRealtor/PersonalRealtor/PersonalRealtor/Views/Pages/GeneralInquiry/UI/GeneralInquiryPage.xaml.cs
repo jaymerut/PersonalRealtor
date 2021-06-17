7fa666ead973bcd9386b7313823b2c3481c5524c
Jayme Rutkoski <jaymerut@gmail.com>
2021-06-16 20:26:05 -0700

    Add General Inquiry email functionality

--- PersonalRealtor/PersonalRealtor/PersonalRealtor/Views/Pages/GeneralInquiry/UI/GeneralInquiryPage.xaml.cs
+++ PersonalRealtor/PersonalRealtor/PersonalRealtor/Views/Pages/GeneralInquiry/UI/GeneralInquiryPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -22,6 +23,28 @@ namespace PersonalRealtor.Views.Pages.GeneralInquiry.UI
         private void SetUpGeneralInquiryPage()
         {
             this.LabelTitle.Text = $"Email {RealtorSingleton.Instance.FullName}: ";
+        }
+
+        public async void ButtonSend_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                var message = new EmailMessage
+                {
+                    Subject = "Subject",
+                    Body = "Body",
+                    To = new List<string>() { "Receipent@test.com" },
+                };
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                // Email is not supported on this device  
+            }
+            catch (Exception ex)
+            {
+                // Some other exception occurred  
+            }
         }
     }
 }
