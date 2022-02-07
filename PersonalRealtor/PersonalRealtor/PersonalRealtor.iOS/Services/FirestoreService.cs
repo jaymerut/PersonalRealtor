dd7dcaf29dda42136f490d48d86d7276da8e97e3
Jayme Rutkoski <jaymerut@gmail.com>
2022-02-06 21:25:31 -0700

    Create FirestoreService classes

---
+++ PersonalRealtor/PersonalRealtor/PersonalRealtor.iOS/Services/FirestoreService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PersonalRealtor.iOS.Services {
+    public static class FirestoreService {
+        public static Firebase.CloudFirestore.Firestore Instance {
+            get {
+                return Firebase.CloudFirestore.Firestore.SharedInstance;
+            }
+        }
+
+        public static void Init() {
+            Firebase.Core.App.Configure();
+        }
+    }
+}
