8adf2bc298374e6160de167872586cd99d05310c
Jayme Rutkoski <jaymerut@gmail.com>
2022-02-17 18:46:07 -0700

    Create IUsersRepository

--- PersonalRealtor/PersonalRealtor/PersonalRealtor/Network/Firestore/Users/IUsersRepository.cs
+++ PersonalRealtor/PersonalRealtor/PersonalRealtor/Network/Firestore/Users/IUsersRepository.cs
@@ -1,5 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace PersonalRealtor.Network.Firestore.Users {
-    public interface IUsersRepository {
+    public interface IUsersRepository<T> {
+        T GetOne(string id);
+        Task<T> GetOneAsync(string id);
+
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
     }
 }
